f8071af079d8cf80e0b383b0ed008adc28e08541
Unknown <orn.fridriks@gmail.com>
2018-10-24 17:59:57 +0000

    skittles

---
+++ HoopDreams/resolvers/index.js
@@ -0,0 +1,10 @@
+const playerResolver = require('./playerResolver');
+
+module.exports = {
+    Query: {
+        ...playerResolver.queries
+    },
+    Mutation: {
+        ...playerResolver.mutations
+    }
+}

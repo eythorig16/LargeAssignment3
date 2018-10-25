b82d2e16aa2d7318449048106f470380c9c05a1f
eythorig16 <eythorig16@ru.is>
2018-10-25 16:44:44 +0000

    added context

--- HoopDreams/resolvers/basketballFieldResolver.js
+++ HoopDreams/resolvers/basketballFieldResolver.js
@@ -4,7 +4,6 @@ module.exports = {
     queries: {
         allBasketballFields: async () => {
             const fields = BasketballField.find({});
-            console.log(fields);
             return fields;
         },
         basketballField: (parent, args) => {
--- HoopDreams/resolvers/playerResolver.js
+++ HoopDreams/resolvers/playerResolver.js
@@ -3,22 +3,33 @@ const { Player } = require('../data/db');
 
 module.exports = {
     queries: {
-        allPlayers: () => {
-            return Player.find({});
+        allPlayers: (root, args, context) => {
+            return new Promise((resolve, reject) => {
+                context.db.Player.find({}, (err, data) => {
+                    resolve(data);
+                });
+            });
+                // Player.find({});
         },
         player: (parent, args) => {
             return allPlayers.find(p => p.id === args.id);
         }
     },
     mutations: {
-        createPlayer: (parent, args) => {
-            const newPlayer = {
-                id: args.input.id,
-                name: args.input.name
-            };
-            Player.create(newPlayer);
-            return newPlayer;
+
+        createPlayer: (root, args, context) => {
+            return new Promise((resolve, rejext) => {
+                context.db.Player.create({
+                    name: args.input.name },
+                    (err, newPlayer) => {
+                        console.log(args.input.name);
+                        resolve(newPlayer)
+                    }
+                )
+            });
         },
+
+
         updatePlayer: (parent, args) => {
             const uPlayer = Player.find(p => p.id === args.id);
             uPlayer.name = args.name;
